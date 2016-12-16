c4ef703c4d800f886312f044479b06a925ffce15
Adrian Au <adrianau@outlook.com>
2016-12-16 05:50:58 +0000

    Add seen feature

--- README.md
+++ README.md
@@ -5,11 +5,17 @@ Maintained by Adrian and Matt.
 ## Features
 - Notifies when a user comes online or goes offline.
 - Notifies when a user connects or disconnects from a voice channel.
+- Keeps track of when a user was last online.
 
 ## Commands
 
 ### Info
 - `!info` Displays this GitHub page.
+
+
+###Seen
+Check when a user was last online (by their username or nickname):
+- `!seen Barry` | Barry was last seen at 8:00pm on Tue 7 Oct 2014
 
 
  - - - -
--- bot.py
+++ bot.py
@@ -1,62 +1,99 @@
 import config
 import discord
-import asyncio
+import json
+import os
+from datetime import datetime
 from discord.ext import commands
 
-import time
-
-# aliases
 bot = commands.Bot(command_prefix = '!')
-status = discord.member.Status
 
-@bot.event # on ready
+@bot.event
 async def on_ready():
-	print('Connected')
-	print('------')
+	init_data()
+	set_server('GGT')
+	set_log('log')
+	print ('Connected')
 
-
-@bot.event # online / offline message
-async def on_member_update(before, after):
-	c = before.server.default_channel
-
-	if (before.status != status.offline) & (after.status == status.offline):
-		await bot.send_message(c, '`' + after.name + ' is now offline' + '`')
-	elif (before.status == status.offline) & (after.status != status.offline):
-		await bot.send_message(c, '`' + after.name + ' is now online' + '`')
-
-
-@bot.event # join / leave channel message
-async def on_voice_state_update(before, after):
-	c = before.server.default_channel
-	bc = before.voice.voice_channel
-	ac = after.voice.voice_channel
-
-	if (bc is not None) & (ac is None):
-		await bot.send_message(c, '`' + after.name + ' left ' + bc.name +'`')
-	elif bc != ac:
-		await bot.send_message(c, '`' + after.name + ' joined ' + ac.name +'`')
-
-
-@bot.command() # shows github link
+@bot.command() # displays github link
 async def info():
-	await bot.say('https://github.com/adrianau/Gideon')
+	await bot.say('https://github.com/adrianau/Gideon/tree/python')
 
+@bot.event # logs online and offline status
+async def on_member_update(before, after):
+	if (before.status != discord.member.Status.offline) & (after.status == discord.member.Status.offline):
+		await bot.send_message(log, bt(after.name + ' is now offline') )
+		update_seen(before)
+	elif (before.status == discord.member.Status.offline) & (after.status != discord.member.Status.offline):
+		await bot.send_message(log, bt(after.name + ' is now online') )
 
-@bot.command() # adds two numbers
-async def add(left : int, right : int):
-	if int(left) & int(right):
-		bot.say(left + right)
-	else: 
-		bot.say('both need to be ints')
-	
+@bot.event # logs joining and leaving voice channels
+async def on_voice_state_update(before, after):
+	if (before.voice.voice_channel is not None) & (after.voice.voice_channel is None):
+		await bot.send_message(log, bt(after.name + ' left ' + before.voice.voice_channel.name) )
+	elif (before.voice.voice_channel != after.voice.voice_channel):
+		await bot.send_message(log, bt(after.name + ' joined ' + after.voice.voice_channel.name) )
 
-@bot.command(pass_context = True) # testing
-async def test(context):
-	member = context.message.author
-	current_time = time.localtime()
-	formatted = time.strftime('%a, %d %b %Y %H:%M:%S GMT', current_time)
-	await bot.say(formatted)
+@bot.command() # checks when member was last online
+async def seen(message):
+	member = server.get_member_named(message)
 
+	if (member is None):
+		await bot.say( bt('User not found') )
+	else:
+		with open(seen_data, 'r') as f:
+			data = json.load(f)
+
+			# check if member is in the data
+			in_data = True if member.name in data else False
+			if (in_data):
+				await bot.say( bt(member.name + ' was last seen on ' + data[member.name]) )
+			else:
+				await bot.say( bt(member.name + ' not seen yet') )
+
+# UTILITY FUNCTIONS #
+
+# sets the global server variable
+def set_server(server_name):
+	global server
+	server = discord.utils.get(bot.servers, name = server_name)
+
+# sets the channel to output the server logs
+def set_log(channel_name):
+	global log
+	log = discord.utils.get(server.channels, name = channel_name)
+
+# initialises data files
+def init_data():
+	global seen_data
+	seen_data = 'seen.json'
+
+	if (os.path.isfile(seen_data)) == False:
+		with open(seen_data, 'w') as f:
+			json.dump({}, f, indent = 4)
+
+# gets current time (formatted)
+def get_time():
+	now = datetime.now()
+	date = now.strftime('%a %d %b %Y')
+	time = now.strftime('%I:%M%p').lstrip('0')
+	return date + " at " + time
+
+# updates seen data
+def update_seen(member):
+
+	# reads and updates json
+	with open(seen_data, 'r') as f:
+		data = json.load(f)
+		data[member.name] = get_time()
+
+	# remove the old json and write new file
+	os.remove(seen_data)
+	with open(seen_data, 'w') as f:
+		json.dump(data, f, indent = 4)
+
+# embeds a message in backticks
+def bt(message):
+	return '`' + message + '`'
 
 # run bot
 bot.run(config.token)
