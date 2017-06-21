fb0d76d5a2e9880b79cc8c9fc7fce4f11eb734b9
Adrian Au <adrianau@outlook.com>
2017-06-21 16:59:57 +0100

    Refactor structure

--- bot.py
+++ bot.py
@@ -1,201 +1,160 @@
-import config
 import discord
+import config
+import random
+import time
 import json
 import os
-import random
-import math
-from datetime import datetime
 from discord.ext import commands
-from discord.member import Status
+from datetime import datetime
+from discord import Status
+from pprint import pprint
 
-bot = commands.Bot(command_prefix = '!')
+bot = commands.Bot(command_prefix = '.')
+bot_files = ['seen.json', 'aliases.json']
+bot_roles = ['voice']
+
+#================================================================================
+# Events
+#================================================================================
 
 @bot.event
 async def on_ready():
-	init_data()
-	init_flags()
-	set_server('GGT')
-	set_log('log')
-	await update_voice_role()
+	await init_bot()
 	print ('Connected')
 
-@bot.command() # displays github link
-async def info():
-	await bot.say('https://github.com/adrianau/Gideon/tree/python')
+@bot.event
+async def on_member_update(before, after):
+	"""Logs when users connect and disconnect from the server"""
+	if not after.bot:
+		if (before.status is not Status.offline) & (after.status is Status.offline):
+			await bot.send_message(bot.log, after.name + ' connected')
+			update_seen(after, datetime.now())
 
-@bot.command() # randomly chooses from a given list
+		elif (before.status is Status.offline) & (after.status is not Status.offline):
+			await bot.send_message(bot.log, after.name + ' disconnected')
+
+@bot.event
+async def on_voice_state_update(before, after):
+	"""Logs when users join and leave a voice channel"""
+	voice_role = discord.utils.get(bot.server.roles, name = 'voice')
+
+	if (before.voice.voice_channel is not None) & (after.voice.voice_channel is None):
+		await bot.send_message(bot.log, after.name + ' left ' + before.voice.voice_channel.name)	
+		await bot.remove_roles(after, voice_role)
+
+	elif (before.voice.voice_channel != after.voice.voice_channel):
+		await bot.send_message(bot.log, after.name + ' joined ' + after.voice.voice_channel.name)
+		await bot.add_roles(after, voice_role)
+
+#================================================================================
+# User commands
+#================================================================================
+
+@bot.command()
+async def info():
+	"""Displays Github link"""
+	await bot.say('https://github.com/adrianau/Gideon')
+
+@bot.command()
 async def choose(*choices : str):
+	"""Randomly chooses from given choices"""
 	if choices:
 		answer = random.choice(choices)
 		chance = '{0:.0f}%'.format(choices.count(answer) / len(choices) * 100)
-		await bot.say( bt(answer + ' (' + chance + ')') )
+		await bot.say(answer + ' (' + chance + ')')
 	else:
-		await bot.say( bt('No inputs given') )
+		await bot.say('No input given')
 
-@bot.event # logs online and offline status
-async def on_member_update(before, after):
-	if (before.bot == False):
-		if (before.status != Status.offline) & (after.status == Status.offline):
-			await bot.send_message(log, bt(after.name + ' is now offline') )
-			update_seen(before)
-		elif (before.status == Status.offline) & (after.status != Status.offline):
-			await bot.send_message(log, bt(after.name + ' is now online') )
-
-@bot.event # logs joining and leaving voice channels
-async def on_voice_state_update(before, after):
-	voice_role = get('voice', server.roles)
-
-	if (before.voice.voice_channel is not None) & (after.voice.voice_channel is None):
-		await bot.send_message(log, bt(after.name + ' left ' + before.voice.voice_channel.name) )
-		await bot.remove_roles(after, voice_role)
-	elif (before.voice.voice_channel != after.voice.voice_channel):
-		await bot.send_message(log, bt(after.name + ' joined ' + after.voice.voice_channel.name) )
-		await bot.add_roles(after, voice_role)
-
-@bot.command(pass_context = True)
-async def votemute(context, member: discord.Member = None):
-	global vote_in_progress
-	
-	# preliminary checks
-	if (member is None):
-		await bot.say ( bt('User not found') )
-
-	if (member.voice.voice_channel is None):
-		await bot.say( bt('User not in a voice channel') )
+@bot.command()
+async def seen(user):
+	"""Outputs when the user last appeared online on the server"""
+	member = member_from_alias(user, bot.server)
+	if member is None:
+		await bot.say('User not found')
 		return
 
-	if (vote_in_progress):
-		await bot.say ( bt('There is a vote in progress') )
-		return
-
-	vote_in_progress = True
-
-	if (member.voice.mute):
-		vote_type = 'unmute'
+	seen_time = find(member.id, 'seen.json')
+	if seen_time:
+		await bot.say(member.name + ' was last seen on ' + format_time(seen_time))
 	else:
-		vote_type = 'mute'
+		await bot.say(member.name + ' not seen yet')
 
-	voice_clients = 0
-	for m in server.members:
-		if (m.voice.voice_channel):
-			voice_clients += 1
+#================================================================================
+# Utility functions
+#================================================================================
 
-	votes_needed = math.floor(voice_clients/2) + 1
-	
-	await bot.say( bt('Votes required to ' + vote_type + ' ' + member.name + ': ' + str(votes_needed + 1)) )
-	result = await vote(context.message, votes_needed)
+def update_seen(member, time):
+	update_file('seen.json', member.id, str(time))
 
-	if (result is None):
-		await bot.say ( bt('Vote has ended') )
-	elif (member.voice.mute):
-		await bot.server_voice_state(member, mute = False)
-		await bot.say( bt(member.name + ' has been unmuted') )
-	elif (member.voice.mute is False):	
-		await bot.server_voice_state(member, mute = True)
-		await bot.say( bt(member.name + ' has been muted') )
+async def update_role(role):
+	"""Updates roles with newest members"""
+	if role.name == 'voice':
+		for member in bot.server.members:
+			if member.voice.voice_channel:
+				await bot.add_roles(member, role)
+			elif role in member.roles:
+				await bot.remove_roles(member, role)
 
-	vote_in_progress = False
+def format_time(dt):
+	"""Formats a datetime string like: 15 May 2017 at 9:01PM"""
+	dto = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S.%f')
+	date = dto.strftime('%a %d %b %Y')
+	time = dto.strftime('%I:%M%p').lstrip('0')
 
-async def vote(message, votes_needed):
-	upvotes = 0
-	voted_users = []
-	await bot.add_reaction(message, '👍' )
+	return date + ' at ' + time
 
-	def reaction_check(reaction, user):
-		nonlocal upvotes
-		nonlocal votes_needed
-
-		if (user.bot == False) & (user not in voted_users):
-			print ('received ' + reaction.emoji + ' from ' + user.name)
-
-			if reaction.emoji == '👍':
-				upvotes += 1
-				voted_users.append(user)
-
-			if (upvotes == votes_needed):
-				return True
-
-	return await bot.wait_for_reaction(timeout = 60, check = reaction_check)
-
-@bot.command() # checks when member was last online
-async def seen(message):
-	member = server.get_member_named(message)
-
-	if (member is None):
-		await bot.say( bt('User not found') )
+def member_from_alias(alias, server):
+	"""Retrieves a member object from a given alias"""
+	id = find(alias.lower(), 'aliases.json')
+	if id:
+		return bot.server.get_member(id)
 	else:
-		with open(seen_data, 'r') as f:
-			data = json.load(f)
+		return None
 
-			# check if member is in the data
-			in_data = True if member.name in data else False
-			if (in_data):
-				await bot.say( bt(member.name + ' was last seen on ' + data[member.name]) )
-			else:
-				await bot.say( bt(member.name + ' not seen yet') )
+#================================================================================
+# File management
+#================================================================================
 
-# UTILITY FUNCTIONS #
+async def init_bot():
+	"""Initialises files and necessary setup"""
+	bot.server = list(bot.connection._servers.values())[0]
+	bot.log = discord.utils.get(bot.server.channels, name = 'log')
 
-# gets an object by name from an iterable list
-def get(object_name, list):
-	return discord.utils.get(list, name = object_name)
+	for file in bot_files:
+		if os.path.isfile(file):
+			print(file + ' found!')
+		else:
+			print(file + ' not found')
+			with open(file, 'w') as f:
+				json.dump({}, f, indent = 4)
 
-# sets the global server variable
-def set_server(server_name):
-	global server
-	server = get(server_name, bot.servers)
+	for role in bot_roles:
+		role = discord.utils.get(bot.server.roles, name = role)
+		await update_role(role)
 
-# sets the channel to output the server logs
-def set_log(channel_name):
-	global log
-	log = get(channel_name, server.channels)
-
-# initialises data files
-def init_data():
-	global seen_data
-	seen_data = 'seen.json'
-
-	if (os.path.isfile(seen_data)) == False:
-		with open(seen_data, 'w') as f:
-			json.dump({}, f, indent = 4)
-
-# initialises global flags
-def init_flags():
-	global vote_in_progress
-	vote_in_progress = False
-
-# gets current time (formatted)
-def get_time():
-	now = datetime.now()
-	date = now.strftime('%a %d %b %Y')
-	time = now.strftime('%I:%M%p').lstrip('0')
-	return date + " at " + time
-
-# updates seen data
-def update_seen(member):
-
-	# reads and updates json
-	with open(seen_data, 'r') as f:
+def update_file(file, key, value):
+	"""Updates a file with a new key/value pair"""
+	with open(file, 'r') as f:
 		data = json.load(f)
-		data[member.name] = get_time()
+		data[key] = value
 
-	# remove the old json and write new file
-	os.remove(seen_data)
-	with open(seen_data, 'w') as f:
-		json.dump(data, f, indent = 4)
+		os.remove(file)
+		with open(file, 'w') as f:
+			json.dump(data, f, indent = 4)
 
-# manually updates the members of the voice role when called
-async def update_voice_role():
-	voice_role = get('voice', server.roles)
-	for member in server.members:
-		if (member.voice.voice_channel):
-			await bot.add_roles(member, voice_role)
-		elif (voice_role in member.roles):
-			await bot.remove_roles(member, voice_role)
+def find(key, file):
+	"""Returns value of a key in a file"""
+	with open(file, 'r') as f:
+		data = json.load(f)
+		found = True if key in data else False
 
-# embeds a message in backticks
-def bt(message):
-	return '`' + message + '`'
+		if found:
+			return data[key]
+		else:
+			return None
 
-# run bot
+#================================================================================
+# Token
+#================================================================================
+
 bot.run(config.token)
